461c88b47752753f3a14a9a0b355fb9920d32869
Cody Cornelison <codysmart@gmail.com>
2019-01-22 21:24:58 -0800

    Added the rest to the initial form.

--- web/displayUser.php
+++ web/displayUser.php
@@ -1,5 +1,8 @@
 <?php
 
-  echo "User Name: ".$_POST['Name']."<br>";
+    echo "User Name: ".$_POST['Name']."<br>";
+    echo "Email: "."<a href=\"mailto:".$_POST['Email']."\">".$_POST['Email']."</a><br>";
+    echo "Major: ".$_POST['Major']."<br>";
+    echo "Comments: ".$_POST['Comments']."<br>";
 
 ?>
