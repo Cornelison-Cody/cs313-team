f37b1c5566e7ce2d1009472511a0e49b3d85d366
Cody Cornelison <codysmart@gmail.com>
2019-01-22 21:37:24 -0800

    Added continents

--- web/displayUser.php
+++ web/displayUser.php
@@ -1,8 +1,14 @@
 <?php
 
+
     echo "User Name: ".$_POST['Name']."<br>";
     echo "Email: "."<a href=\"mailto:".$_POST['Email']."\">".$_POST['Email']."</a><br>";
     echo "Major: ".$_POST['Major']."<br>";
-    echo "Comments: ".$_POST['Comments']."<br>";
+    echo "Continents Visited:";
+    foreach ($_POST['Continents'] as $continent) {
+        echo $continent." ";
+    }
+    echo "<br>Comments: ".$_POST['Comments']."<br>";
+
 
 ?>
